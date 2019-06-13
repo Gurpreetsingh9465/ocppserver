e3bc32a4b913074f6400d96c6c6bd13449c3a790
gurpreet singh <amansingh9569@gmail.com>
2019-06-13 14:32:42 +0530

    tomcat running

--- src/main/java/ocpp/OcppApplication.java
+++ src/main/java/ocpp/OcppApplication.java
@@ -3,14 +3,21 @@ package ocpp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @EnableJpaRepositories("ocpp.repo")
 @SpringBootApplication
-public class OcppApplication {
+public class OcppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcppApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(OcppApplication.class);
+    }
 }
